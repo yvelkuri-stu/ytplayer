a6319e271742d930bfe3fd7c23b2b205bcdee334
yvelkuri-stu <yvelkuri@stu.calums.edu>
2021-06-10 17:22:00 -0700

    Updated callback function

--- src/routes/callback.js
+++ src/routes/callback.js
@@ -1,14 +1,33 @@
 
-const secret = import.meta.env.VITE_CLIENT_SECRET; 
-const client = import.meta.env.VITE_CLIENT_ID; 
-const target = 'https://github.com/login/oauth/authorize'
+const client_secret = import.meta.env.VITE_CLIENT_SECRET; 
+const client_id = import.meta.env.VITE_CLIENT_ID; 
+const token_url = 'https://github.com/login/oauth/access_token'
 
 
 export async function get(request){
     const session=123;
     const code = request.query.get('code')
+    const access_token = await getToken(code);
+    //get token from commander
     return{
-        body:'received code'+code
+        body:'received code'+code + 'token::'+access_token
         
     }
+}
+
+function getToken(code){
+
+    return fetch(`${token_url}`,{
+        method:'POST',
+        headers:{
+            'content-type': 'application/json',
+            'accept': 'application/json'
+        },
+        body: JSON.stringify({
+            client_id,
+            client_secret,
+            code
+
+        })
+    }).then(r => r.json()).then(r => r.access_token);
 }
