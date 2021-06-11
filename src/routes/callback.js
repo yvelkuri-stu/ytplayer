5814baa2821bf09d7242c95c465dbbc4c2743e87
yvelkuri-stu <yvelkuri@stu.calums.edu>
2021-06-10 17:31:21 -0700

    Added github user url

--- src/routes/callback.js
+++ src/routes/callback.js
@@ -2,19 +2,29 @@
 const client_secret = import.meta.env.VITE_CLIENT_SECRET; 
 const client_id = import.meta.env.VITE_CLIENT_ID; 
 const token_url = 'https://github.com/login/oauth/access_token'
+const user_url = 'https://api.github.com/user'
 
 
 export async function get(request){
     const session=123;
     const code = request.query.get('code')
     const access_token = await getToken(code);
+    const user = await getUser(access_token);
     //get token from commander
     return{
-        body:'received code'+code + 'token::'+access_token
+        body:JSON.stringify(user, null, 2)
         
     }
 }
 
+function getUser(access_token){
+    return fetch(user_url,{
+        headers:{
+            accept: 'application/json',
+            authorization: `Bearer ${access_token}`
+        }
+    }).then(r => r.json());
+}
 function getToken(code){
 
     return fetch(`${token_url}`,{
